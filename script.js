c6ff163ff2029d4105508df0692b416c0b3f4178
Omkar <140525172+omkaripte@users.noreply.github.com>
2024-06-11 18:22:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+ //--------------For Click Feature (About Section)--------------//
+
         var tablinks = document.getElementsByClassName ("tablinks");
         var tabcontents = document.getElementsByClassName ("tabcontents");
 
@@ -15,7 +17,15 @@
         }
 
 
-        // ----------For SideMenu (below 600px)---------
+ // ---------Typed.js function for text animation (Home Section)--------//
+        
+        var typed = new Typed('#element', {
+            strings: ['Web Designer','Graphic Designer','Web Developer'],
+            typeSpeed: 50,
+          });
+
+
+// ----------For SideMenu (below 600px)(Home Section)---------//
 
         function openmenu() {
             sidemenu.style.right = "0";
@@ -26,5 +36,31 @@
         }
 
 
-            
-                
+// ----------For Reset Form after sending details (Contact Section)----------//
+
+    var form = document.getElementById('form')
+        form.addEventListener('submit', e =>{
+            e.preventDefault()
+            fetch('https://api.web3forms.com/submit',{ 
+                method: 'POST' ,body: new FormData(form)})
+            .then(response => {
+                window.alert("Message sent successfully!")
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error))
+        })
+
+        // var form = document.getElementById('form')
+        // form.addEventListener('submit', e =>{
+        //     e.preventDefault()
+        //     fetch('https://api.web3forms.com/submit',{ 
+        //         method: 'POST' ,body: new FormData(form)})
+        //     .then(response => {
+        //         msg.innerHTML = "Message sent successfully"
+        //         setTimeout(function(){
+        //             msg.innerHTML = ""
+        //         },5000)
+        //         form.reset()
+        //     })
+        //     .catch(error => console.error('Error!', error))
+        // })
